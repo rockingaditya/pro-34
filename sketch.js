4df51401edbcd4509ecdd04c534f38c8682b9273
Aditya Raj <68543352+rockingaditya@users.noreply.github.com>
2020-10-27 18:17:17 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,57 @@
+var dog,sadDog,happyDog;
+var database;
+var foodS,foodStock;
+
+function preload(){
+   sadDog=loadImage("Images/dogImg.png");
+   happyDog=loadImage("Images/dogImg1.png");
+  }
+
+//Function to set initial environment
+function setup() {
+  database=firebase.database();
+  createCanvas(500,500);
+
+  dog=createSprite(250,300,150,150);
+  dog.addImage(sadDog);
+  dog.scale=0.15;
+
+  foodStock=database.ref('Food');
+  foodStock.on("value",readStock);
+  textSize(20); 
+}
+
+// function to display UI
+function draw() {
+  background(46,139,87);
+ 
+  if(keyWentDown(UP_ARROW)){
+    writeStock(foodS);
+    dog.addImage(happyDog);
+  }
+
+  drawSprites();
+  fill(255,255,254);
+  stroke("black");
+  text("Food remaining : "+foodS,170,200);
+  textSize(13);
+  text("Note: Press UP_ARROW Key To Feed Drago Milk!",130,10,300,20);
+}
+
+//Function to read values from DB
+function readStock(data){
+  foodS=data.val();
+}
+
+//Function to write values in DB
+function writeStock(x){
+  if(x<=0){
+    x=0;
+  }else{
+    x=x-1;
+  } 
+  database.ref('/').update({
+    Food:x
+  })
+}
+
